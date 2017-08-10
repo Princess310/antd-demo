8da8c33bf89a1a7fed1a1d605b5899e3c0783ca5
princess310 <837478514@qq.com>
2017-08-10 17:26:00 +0800

    . update share business page

--- app/share/containers/ShareBusinessPage/index.js
+++ app/share/containers/ShareBusinessPage/index.js
@@ -15,6 +15,7 @@ import UserHeaderBar from 'components/UserHeaderBar';
 import FlexSB from 'components/FlexSB';
 import FlexCenter from 'components/FlexCenter';
 import ShareMomentCard from 'components/MomentCard/ShareMomentCard';
+import TouchLoader from 'components/TouchLoader';
 
 import DownloadBar from 'share/components/DownloadBar';
 import DownloadQrcode from 'share/components/DownloadQrcode';
@@ -47,12 +48,27 @@ const ActionWrapper = styled(FlexSB)`
   z-index: 20;
 `
 
+const SectionTitle = styled.div`
+  height: 0.8rem;
+  font-size: 0.3rem;
+  line-height: 0.8rem;
+  text-align: center;
+  background-color: ${pallete.white};
+`;
+
 const TabPane = Tabs.TabPane;
 export class ShareBusinessPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   state = {
     title: '动态详情',
     type: 'business',
     moment: null,
+    businessType: 'status',
+    otherList: {
+      page: 1,
+      loading: false,
+      list: false,
+      hasNext: false,
+    },
   }
 
   handleDownloadInfo = () => {
@@ -70,6 +86,7 @@ export class ShareBusinessPage extends React.PureComponent { // eslint-disable-l
       const { category, reward_as, source_type, share_user } = data;
       let title = (category === '3' || reward_as === '2') ? '需求动态详情' : ((category === '0' || reward_as === '1') ? '供应动态详情' : '动态详情');
       const type = (category === '3' || reward_as === '2' || category === '0' || reward_as === '1') ? 'business' : 'communication';
+      const businessType = (category === '3' || reward_as === '2') ? 'demand' : ((category === '0' || reward_as === '1') ? 'supplier' : 'status');
 
       // if it is CMS
       if (source_type === 1) {
@@ -80,13 +97,93 @@ export class ShareBusinessPage extends React.PureComponent { // eslint-disable-l
         title,
         type,
         moment: data,
+        businessType,
       });
 
       shareConfig.share('momment', data, share_user);
+
+      if (type !== 'communication') {
+        this.getOtherStatus(data.id, 1);
+      }
     });
   }
+
+  getOtherStatus = (id, page) => {
+    const self = this;
+    const { otherList } = this.state;
+
+    request.doGet('moments/analogy', {
+      moments_id: id,
+      page,
+    }).then((res) => {
+      const { list, page } = res;
+      let hasNext = true;
+
+      if (page) {
+        if (page.current_page >= page.page_count) {
+          hasNext = false;
+        }
+      } else {
+        hasNext = false;
+      }
+
+      self.setState({
+        otherList: {
+          page: (page ? page.current_page : 1),
+          loading: false,
+          list: otherList.list ? [...otherList.list, ...list] : list,
+          hasNext,
+        },
+      });
+    });
+  }
+
+  onEndReached = () => {
+    const { moment, otherList } = this.state;
+
+    this.getOtherStatus(moment.id, otherList.page + 1);
+  }
+
   render() {
-    const { title, moment, type } = this.state;
+    const { title, moment, type, businessType, otherList } = this.state;
+    const loading = otherList.loading;
+    const hasNext = otherList.hasNext;
+    const yearList = [];
+
+    const listView = (otherList.list && otherList.list.length > 0) ? otherList.list.map((moment) => {
+      const date = new Date(moment.created_at * 1000);
+      const year = date.getFullYear();
+      if (yearList.findIndex((y) => y === year) === -1) {
+        yearList.push(year);
+
+        return (
+          <div key={moment.id}>
+            <section
+              style={{
+                paddingLeft: '0.28rem',
+                color: pallete.theme,
+                fontSize: '0.36rem',
+                height: '0.64rem',
+                lineHeight: '0.64rem',
+                backgroundColor: pallete.white,
+              }}
+            >{year}</section>
+            <ShareMomentCard
+              moment={moment}
+              from="list"
+              type="business"
+            />
+          </div>
+        );
+      }
+
+      return <ShareMomentCard
+        key={moment.id}
+        moment={moment}
+        from="list"
+        type="business"
+      />
+    }) : null;
 
     return (
       <div>
@@ -96,18 +193,27 @@ export class ShareBusinessPage extends React.PureComponent { // eslint-disable-l
         >
           {title.length > 18 ? `${title.substring(0, 15)}...` : title}
         </NavBar>
-        <AppContent style={{ paddingBottom: '1.28rem' }}>
-            {
-              moment ? (
+        <TouchLoader
+          initializing={0}
+          hasMore={hasNext}
+          loading={loading}
+          onLoadMore={this.onEndReached}
+          autoLoadMore={true}
+          className="tloader app-content"
+          style={{ paddingBottom: '1.28rem' }}
+        >
+          {
+            moment ? (
+              <div>
                 <div>
-                  <div>
-                    <ShareMomentCard
-                      moment={moment}
-                      from="detail"
-                      type={type}
-                    />
-                  </div>
-                  <WhiteSpace size="md" />
+                  <ShareMomentCard
+                    moment={moment}
+                    from="detail"
+                    type={type}
+                  />
+                </div>
+                <WhiteSpace size="md" />
+                {type === 'communication' &&
                   <Tabs
                     defaultActiveKey="1"
                     animated={false}
@@ -164,12 +270,28 @@ export class ShareBusinessPage extends React.PureComponent { // eslint-disable-l
                       </div>
                     </TabPane>
                   </Tabs>
-                </div>
-              ) : null
-            }
-            <WhiteSpace size="md" />
-            <DownloadQrcode />
-        </AppContent>
+                }
+
+                {businessType === 'demand' && (
+                  <div>
+                    <SectionTitle>其他类似需求</SectionTitle>
+                    <WhiteSpace size="md" />
+                    {listView}
+                  </div>
+                )}
+                {businessType === 'supplier' && (
+                  <div>
+                    <SectionTitle>其他类似供应</SectionTitle>
+                    <WhiteSpace size="md" />
+                    {listView}
+                  </div>
+                )}
+              </div>
+            ) : null
+          }
+          <WhiteSpace size="md" />
+          <DownloadQrcode />
+        </TouchLoader>
         <DownloadBar />
       </div>
     );
