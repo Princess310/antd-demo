14ce2c7e2c1d336a44b337fb9a453342382efe82
princess310 <837478514@qq.com>
2017-09-01 17:36:32 +0800

    . test open install

--- app/share/containers/App/index.js
+++ app/share/containers/App/index.js
@@ -56,7 +56,7 @@ export default class App extends React.PureComponent { // eslint-disable-line re
 
     var m = new OpenInstall({
       /*appKey必选参数，openinstall平台为每个应用分配的ID,在开发者平台可查看此appKey*/
-      appKey : ewrjhz
+      appKey : 'ewrjhz',
       /*可选参数，自定义android平台的apk下载文件名，只有apk在openinstall托管时才有效；
         个别andriod浏览器下载时，中文文件名显示乱码，请慎用中文文件名！*/
       //apkFileName : "OpenInstallDemo-v2-1.1.1.apk",
