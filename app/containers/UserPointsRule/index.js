80f42cc938f355d4d84b6130e2e21cedb7ebcbfa
princess310 <837478514@qq.com>
2017-10-12 11:28:13 +0800

    . update the agree page info

--- app/containers/UserPointsRule/index.js
+++ app/containers/UserPointsRule/index.js
@@ -30,7 +30,7 @@ export class UserPointsRule extends React.PureComponent { // eslint-disable-line
           <section>2.在动态频道有效评论、点赞、分享会得到积分奖励</section>
           <section>3.在生意频道发布需求或供应会消耗相应积分</section>
           <section> 4.发布的违法、诈骗、违规的信息会对动态进行删除，并且双倍扣除积分，情节严重者，可能会面临封号处理，请谨慎对待</section>
-          <section>注意：本规则一切解释权与修改权归《成都阿里健网络科技有限公司》所有</section>
+          <section>注意：本规则一切解释权与修改权归《四川阿里健网络科技有限公司》所有</section>
         </AppContent>
       </div>
     );
